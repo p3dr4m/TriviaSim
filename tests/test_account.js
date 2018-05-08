b309cbf83676be9640187d377a470aec0f1f856e
p3dr4m <seenkim@gmail.com>
2018-05-07 17:57:54 -0700

    named test files properly

--- tests/test_account.js
+++
@@ -1,8 +0,0 @@
-let account = require('../models/account')
-
-describe('testing account.js', () => {
-  let accInstance = new account.Account()
-  test('account login', () => {
-    expect(accInstance.login('pedram', 'password')).toBeTruthy()
-  })
-})
