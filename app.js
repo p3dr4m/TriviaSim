e992c23047aa4a51b8318d632e0c4c628e4650a8
Pedram Nazari <p.nazari2@gmail.com>
2018-05-13 20:17:47 -0700

    Readded yarn.lock because slugignore fixes heroku problem (#66)
    * add documentation
    
    * add JSDoc comments
    
    * tests
    
    * named test files properly
    
    * renamed test files so that jest can read them
    
    * AAAHHH
    
    * removed commented code
    
    * deleted commented code
    
    * moved tests to correct files; created test file templates
    
    * moved tests to correct files; created test file templates
    
    Create separate test files and add more documentation (#32)
    
    * add documentation
    
    * add JSDoc comments
    
    * add documentation for new routes
    
    * create test files and add more documentation
    
    * deleted old test file
    
    * add documentation
    
    * tests
    
    * add documentation for new routes
    
    * named test files properly
    
    * renamed test files so that jest can read them
    
    * AAAHHH
    
    * create test files and add more documentation
    
    * account.js tests
    
    * fix database env url
    
    * add query to clear the database before running tests
    
    * fix spelling error
    
    * moved tests to correct files; created test file templates
    
    * add view for create question
    
    * JSDoc documentation for routes in app.js
    
    * Wrote tests for validate password, also fixed docstring for Account.validatePassword()
    
    * tests for validating username
    
    * changed validatePassword to match mainrepo
    
    * deleted unneed test files, moved code from question.test.js to questions.test.js
    
    * deleted unused required modules in account.test.js
    
    * Refactored "opentdb.test.js" tests to work properly
    Edited "userQuestions.test.js" to conform to JavaScript Standard Style
    Added a dummy test to "users.test.js"
    
    * Update LICENSE
    
    * Update LICENSE
    
    * editted users.test.js
    
    * Deprecated save/load JSON methods
    
    * Added yarn.lock for yarn package manager users
    
    * MINOR: Made changes to models so that tests can pass properly everytime
    
    * Deleted yarn.lock to fix heroku build failures
    
    * Edited Users class so that it creates the 'users_data.json' file inside the models folder
    
    * Delete console.log in executeQuery()
    
    * Refactored account.js and tests to work properly (I broke it) Also editted app.js to catch errors properly
    
    * Made createQuestion() more readable, changed account.test.js, delete main.test.js
    
    * Tests for executeQuery()
    Refactored Account.saveCurrentScore() with new timestamp and a readable SQL query
    Tests for Account.saveCurrentScore()
    Most DB queries now properly remove themselves after the database after tests have finished
    Better tests for Account.login() and Account.register()
    Refactored createQuestion() to be more readable and userID is converted to string so that there no SQL query errors
    
    * Created more tests for users.test.js for 100% lines covered
    
    * Added jsdoc-http-plugin and reconfigured jsdoc.conf.json; also I regenerated the out folder
    
    * readded yarn.lock
    
    * Removed conflict headers in app.js

--- app.js
+++ app.js
@@ -142,14 +142,11 @@ app.post('/storeuser', (request, response) => {
   }
 })
 
-<<<<<<< HEAD
-=======
 /**
  * @desc Function creates a new sessionID and a new User using the Account class, allowing users to play without an account.
  * @param {Object} request - Node.js request object
  * @param {Object} response - Node.js response object
  */
->>>>>>> 583c21c45baf7f025d85ca77ea3baaf26a91a671
 app.post('/playAsGuest', (request, response) => {
   let sessionID = request.session.id.toString()
   let newUser = new account.Account(request.body.username)
